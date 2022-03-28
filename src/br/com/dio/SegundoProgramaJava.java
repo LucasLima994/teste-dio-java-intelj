2063b93abfdaae3012757eb0c71a2ef7c2bed522
Lucas Lima <lucassilva_.12@hotmail.com>
2022-03-28 01:02:03 -0300

    Criando Class Livro

--- src/br/com/dio/SegundoProgramaJava.java
+++ src/br/com/dio/SegundoProgramaJava.java
@@ -9,12 +9,44 @@ public class SegundoProgramaJava {
         System.out.println(gato);
 
 
-
-
-
+        Livro Livro1 = new Livro("Harry Potter e o Calice de Fogo",700);
+        System.out.println(Livro1);
 
        /* int a = 5;
         int b = 3;
         System.out.println("Hello World " +  (a+b));*/
+    }
+}
+class Livro{
+    private String nome;
+    private String npag;
+
+    public Livro(String nome, int npag) {
+        this.nome = nome;
+        this.npag = String.valueOf(npag);
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npag='" + npag + '\'' +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNpag() {
+        return npag;
+    }
+
+    public void setNpag(String npag) {
+        this.npag = npag;
     }
 }
